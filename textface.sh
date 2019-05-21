25b844c7a1c084bf22eaa31701fad4db3fddc4ac
d3suu <d3s.prv@mail.ru>
2019-05-21 20:52:38 +0200

    Easier finding

--- textface.sh
+++ textface.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 faces=$HOME/.config/textfaces/faces.txt
-cat $faces | dmenu -l 5 | tr '\n' ' ' | xclip -selection c
+cat $faces | dmenu -l 5 | sed 's/\w* - //' | tr '\n' ' ' | xclip -selection c
